928d3e8ef3ef2175f97e5dde4f197b82628579e4
vdptrv <vptrv.pr@gmail.com>
2021-01-10 20:58:06 +0300

    init

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,62 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <div id="app">
+        <v-carousel cycle interval="3000">
+            <v-carousel-item
+                    v-for="(item, i) in items"
+                    :key="i"
+            >
+                <v-sheet
+                        height="100%"
+                        tile
+                >
+                    <v-row
+                            class="fill-height"
+                            align="center"
+                            justify="center"
+                    >
+                        <div class="display-3" style="display: flex;justify-content: center;width: 100%;height: 100%;">
+                            <img style="height: 100%" :src="item.src">
+                        </div>
+                    </v-row>
+                </v-sheet>
+            </v-carousel-item>
+        </v-carousel>
+        <HelloWorld msg="Полина Романица (Содержанка)"/>
+    </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld.vue'
 
-export default {
-  name: 'App',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        data() {
+            return {
+                carousel: 0,
+                items: [
+                    { src: 'https://sun9-75.userapi.com/impg/lYawBanESmMvQwrl3BUMpvkueCfsv89j6UE9Zw/pLH_aNDOMp4.jpg?size=828x1002&quality=96&proxy=1&sign=103cb9dc0e5b8c831be85931b5183e2c&type=album', },
+                    { src: 'https://sun9-63.userapi.com/impg/2m4LtUdGq-uDDVCERnsuItNjnry-jQmIS4a-4w/ob0ZH8cxgVg.jpg?size=828x824&quality=96&proxy=1&sign=0328a609d4da34bd5d286ea304e96204&type=album', },
+                    { src: 'https://sun9-7.userapi.com/impg/ltdcs3LYxXK-e3jieKpgqbACS2vXgHz5G8vI0A/wS3iudubGpc.jpg?size=828x864&quality=96&proxy=1&sign=70955d2c37129da1bc54da53fba49b7b&type=album', },
+                ],
+            }
+        }
+    }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+    .v-window__next {
+        right: 0;
+    }
+
+    #app {
+        font-family: Avenir, Helvetica, Arial, sans-serif;
+        -webkit-font-smoothing: antialiased;
+        -moz-osx-font-smoothing: grayscale;
+        text-align: center;
+        color: #2c3e50;
+        margin-top: 60px;
+    }
 </style>
--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,59 @@
 <template>
   <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+    <h1 style="margin-top: 30px">{{ msg }}</h1>
+    <h4>
+      Готова ублажать и любить за ваши деньги
+    </h4>
+
+    <div>
+      <h3>О себе:</h3>
+      <h5>...</h5>
+    </div>
+
+    <div>
+      <h3>Характеристики</h3>
+      <div class="row mt-2">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <v-simple-table>
+            <template v-slot:default>
+              <tbody>
+              <tr>
+                <td style="font-size: 20px">Рост</td>
+                <td style="font-size: 20px">&mdash;</td>
+              </tr>
+              <tr>
+                <td style="font-size: 20px">Вес</td>
+                <td style="font-size: 20px">&mdash;</td>
+              </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+    </div>
+
+    <div>
+      <h3>Цена</h3>
+      <div class="row mt-2">
+        <div class="col-md-3"></div>
+        <div class="col-md-6">
+          <v-simple-table>
+            <template v-slot:default>
+              <tbody>
+              <tr>
+                <td style="font-size: 20px">Неделя</td>
+                <td style="font-size: 20px">250 000 руб.</td>
+              </tr>
+              </tbody>
+            </template>
+          </v-simple-table>
+        </div>
+        <div class="col-md-3"></div>
+      </div>
+    </div>
+
   </div>
 </template>
 
